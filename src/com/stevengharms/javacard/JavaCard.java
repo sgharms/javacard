3282a1f38d72f53421aab8c81f81ddf9742fdc95
Steven G. Harms <stharms@cisco.com>
2010-06-05 07:59:06 -0700

    Basic layout for model

--- src/com/stevengharms/javacard/JavaCard.java
+++ src/com/stevengharms/javacard/JavaCard.java
@@ -2,6 +2,18 @@ package com.stevengharms.javacard;
 
 public class JavaCard{
 	public static void main(String[] args) {
-		System.out.println("Hello, Javacard");
+		JavaCard jc = new JavaCard();
 	}
+	
+	/* 
+				CONSTRUCTORS
+	 */
+	
+	public JavaCard(){
+		System.out.println("Constructor");
+	}
+	
+	/* 
+				METHODS
+	 */
 }
