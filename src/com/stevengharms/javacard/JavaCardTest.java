4dd3076066c67e1fa72c2909e7657a284ec670f2
Steven G. Harms <stharms@cisco.com>
2010-06-06 00:23:07 -0700

    More helpful logging from junit
    test cases are simplified

--- src/com/stevengharms/javacard/JavaCardTest.java
+++ src/com/stevengharms/javacard/JavaCardTest.java
@@ -4,4 +4,13 @@ import junit.framework.*;
 
 
 public class JavaCardTest extends TestCase {
+    public void testOne()
+    {
+        assertTrue(4 == (2*2));
+    }
+
+	public void testTwo()
+	{
+		fail("ralph");
+	}
 }
