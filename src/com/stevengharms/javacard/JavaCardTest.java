c658f8d1d5d82b9b1d0a1fa75f23e04e8f46f354
Steven G. Harms <stharms@cisco.com>
2010-06-05 22:40:22 -0700

    Test class source file.

---
+++ src/com/stevengharms/javacard/JavaCardTest.java
@@ -0,0 +1,16 @@
+package com.stevengharms.javacard;
+
+import junit.framework.*;
+
+
+public class JavaCardTest extends TestCase {
+    public void testOne()
+    {
+        assertTrue( "TestExample", true );
+    }
+
+	public void testTwo()
+	{
+		assertTrue("Razzle", false);
+	}
+}
