86f72df9240529eef48ef485a174331e2292de8b
Steven G. Harms <stharms@cisco.com>
2010-06-06 12:30:11 -0700

    Cleaned up directory, reliably getting error

--- src/test/java/com/stevengharms/javacard/JavaCardTest.java
+++ src/test/java/com/stevengharms/javacard/JavaCardTest.java
@@ -1,7 +1,7 @@
 package com.stevengharms.javacard;
 
-import org. junit.Before;
-import org. junit.Test;
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class JavaCardTest {
