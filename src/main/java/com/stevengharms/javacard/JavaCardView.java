2f620a6a0ee85ab026bc586a3ee9cdf975fc3197
Steven G. Harms <stharms@cisco.com>
2010-08-03 22:01:23 -0700

    Abstracted button names
    Basic GUI

--- src/main/java/com/stevengharms/javacard/JavaCardView.java
+++ src/main/java/com/stevengharms/javacard/JavaCardView.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JavaCardView{
+	public static String[] EAST_PANEL_BUTTON_TITLES = {"Add","Delete"};
+	public static String[] WEST_PANEL_LABEL_TITLES = {"Question:","Answer:"};
+	public static String[] SOUTH_PANEL_BUTTON_TITLES = {"Back", "Correct", "Incorrect", "Forward", "Quit"};
+	
 	public JavaCardView(){
 		// Top level container
 		JFrame frame = new JFrame();
@@ -11,25 +15,25 @@ public class JavaCardView{
 		
 		// East container
 		JPanel eastButtonPanel = new JPanel(new GridLayout(2,1));
-		eastButtonPanel.setSize(120,120);
+		eastButtonPanel.setSize(20,20);
 	    configureButtons(
-	      eastButtonPanel, new String[]{"Add","Delete"});
+	      eastButtonPanel, this.EAST_PANEL_BUTTON_TITLES);
 		
 		
 		// West container
 		JPanel westTextPanel = new JPanel(new GridLayout(2,1) );
 		westTextPanel.setSize(200,200);
 		configureTextAreas(
-			westTextPanel, new String[]{"Question","Answer"});
+			westTextPanel, this.WEST_PANEL_LABEL_TITLES);
 			
 		//South container
 		JPanel southNavigationPanel = new JPanel();
 		configureButtons(
-		  southNavigationPanel, new String[]{"Back", "Right", "Wrong", "Forward"});
+		  southNavigationPanel, this.SOUTH_PANEL_BUTTON_TITLES);
 		
 		// Attach the components 
 		frame.getContentPane().add(BorderLayout.EAST, eastButtonPanel);
-		frame.getContentPane().add(BorderLayout.WEST, westTextPanel);
+		frame.getContentPane().add(BorderLayout.CENTER, westTextPanel);
 		frame.getContentPane().add(BorderLayout.SOUTH, southNavigationPanel);
 
 		// Read 'em and weep
@@ -44,7 +48,17 @@ public class JavaCardView{
 	}
 	public Container configureTextAreas(Container c, String[] names){
 		for (int i=0; i< names.length; i++){
-			c.add(new JTextArea(names[i]));
+			JTextArea jta = new JTextArea();
+			jta.setLineWrap(true);
+			jta.setPreferredSize(new Dimension(10,10));
+			jta.setBorder(BorderFactory.createLineBorder(Color.black));
+
+			JLabel jl = new JLabel(names[i]);
+			jl.setSize(5,1);
+			jl.setLabelFor(jta);
+			
+			c.add(jl);
+			c.add(jta);
 		}
 		return c;
 	}
