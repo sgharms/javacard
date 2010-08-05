4f384a05c650ac348538bb7854159c4d68cada5c
Steven G. Harms <stharms@cisco.com>
2010-08-05 08:40:39 -0700

    Broke out the GUI components into being iVars.  Looks more Cocoa-like this way anyway

--- src/main/java/com/stevengharms/javacard/JavaCardView.java
+++ src/main/java/com/stevengharms/javacard/JavaCardView.java
@@ -4,9 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JavaCardView{
-	public static String[] EAST_PANEL_BUTTON_TITLES = {"Add","Delete"};
-	public static String[] WEST_PANEL_LABEL_TITLES = {"Question:","Answer:"};
-	public static String[] SOUTH_PANEL_BUTTON_TITLES = {"Back", "Correct", "Incorrect", "Forward", "Quit"};
+	private JButton button_add 		= new JButton("Add");
+	private JButton button_del 		= new JButton("Delete");	
+
+	private JTextArea jtext_ques	= new JTextArea("Question:");
+	private JTextArea jtext_answer 	= new JTextArea("Answer:");
+
+	private JButton button_back 	= new JButton("<");
+    private JButton button_right 	= new JButton("Right!");
+    private JButton button_wrong 	= new JButton("Wrong!");
+	private JButton button_forward 	= new JButton(">");
+    private JButton button_exit 	= new JButton("Exit");
+
 	
 	public JavaCardView(){
 		// Top level container
@@ -17,19 +26,19 @@ public class JavaCardView{
 		JPanel eastButtonPanel = new JPanel(new GridLayout(2,1));
 		eastButtonPanel.setSize(20,20);
 	    configureButtons(
-	      eastButtonPanel, this.EAST_PANEL_BUTTON_TITLES);
+	      eastButtonPanel, new JButton[]{button_add,button_del} );
 		
 		
 		// West container
 		JPanel westTextPanel = new JPanel(new GridLayout(2,1) );
 		westTextPanel.setSize(200,200);
 		configureTextAreas(
-			westTextPanel, this.WEST_PANEL_LABEL_TITLES);
+			westTextPanel, new JTextArea[]{jtext_ques, jtext_answer});
 			
 		//South container
 		JPanel southNavigationPanel = new JPanel();
 		configureButtons(
-		  southNavigationPanel, this.SOUTH_PANEL_BUTTON_TITLES);
+		  southNavigationPanel, new JButton[]{button_back, button_right, button_wrong, button_forward, button_exit});
 		
 		// Attach the components 
 		frame.getContentPane().add(BorderLayout.EAST, eastButtonPanel);
@@ -40,25 +49,27 @@ public class JavaCardView{
 		frame.setVisible(true);
 	}
 	
-	public Container configureButtons(Container c, String[] names){
-		for (int i=0; i< names.length; i++){
-			c.add(new JButton(names[i]));
+	public Container configureButtons(Container c, JButton[] jcomps){
+		for (int i=0; i< jcomps.length; i++){
+			c.add(jcomps[i]);
 		}
 		return c;
 	}
-	public Container configureTextAreas(Container c, String[] names){
-		for (int i=0; i< names.length; i++){
+	public Container configureTextAreas(Container c, JTextArea[] jcomps){
+		for (int i=0; i< jcomps.length; i++){
 			JTextArea jta = new JTextArea();
 			jta.setLineWrap(true);
 			jta.setPreferredSize(new Dimension(10,10));
 			jta.setBorder(BorderFactory.createLineBorder(Color.black));
 
-			JLabel jl = new JLabel(names[i]);
+			JLabel jl = new JLabel("TODO");
+			System.out.println(jcomps[i]);
 			jl.setSize(5,1);
 			jl.setLabelFor(jta);
 			
 			c.add(jl);
 			c.add(jta);
+			jcomps[i]=jta;
 		}
 		return c;
 	}
